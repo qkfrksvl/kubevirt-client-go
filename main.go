c2a7f897e47ee5a9443bd54efc89b897f44f40e1
qkfrksvl <qkfrksvl@gmail.com>
2022-11-08 10:50:05 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	k8smetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+	"kubevirt.io/client-go/kubecli"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+type VM struct {
+	Name    string
+	Power   string
+	CPU     uint32
+	Memory  int
+	Network string
+	Disk    int
+	GuestOS string
+	IP      string
+}
+
+func main() {
+
+	// kubecli.DefaultClientConfig() prepares config using kubeconfig.
+	// typically, you need to set env variable, KUBECONFIG=<path-to-kubeconfig>/.kubeconfig
+	clientConfig := kubecli.DefaultClientConfig(&pflag.FlagSet{})
+
+	// retrive default namespace.
+	namespace, _, err := clientConfig.Namespace()
+	if err != nil {
+		log.Fatalf("error in namespace : %v\n", err)
+	}
+
+	// get the kubevirt client, using which kubevirt resources can be managed.
+	virtClient, err := kubecli.GetKubevirtClientFromClientConfig(clientConfig)
+	if err != nil {
+		log.Fatalf("cannot obtain KubeVirt client: %v\n", err)
+	}
+
+	// Fetch list of VMs & VMIs
+	_, err = virtClient.VirtualMachine(namespace).List(&k8smetav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("cannot obtain KubeVirt vm list: %v\n", err)
+	}
+	_, err = virtClient.VirtualMachineInstance(namespace).List(&k8smetav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("cannot obtain KubeVirt vmi list: %v\n", err)
+	}
+
+	GetVM(virtClient, "test02")
+
+	vmList := func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hi"))
+	}
+	http.HandleFunc("/vmlist", vmList)
+	http.HandleFunc("/", httpVMList)
+	http.ListenAndServe(":8000", nil)
+
+}
+
+func GetVM(vClient kubecli.KubevirtClient, name string) *VM {
+	v := &VM{}
+
+	vm, err := vClient.VirtualMachine("default").Get(name, &k8smetav1.GetOptions{})
+	if err != nil {
+		klog.Fatal(err)
+	}
+	vmi, err := vClient.VirtualMachineInstance("default").Get(name, &k8smetav1.GetOptions{})
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	v.Name = vm.Name
+	v.Power = fmt.Sprint(vm.Status.PrintableStatus)
+	v.CPU = vm.Spec.Template.Spec.Domain.CPU.Cores
+	tempMem := vm.Spec.Template.Spec.Domain.Resources.Requests.Memory().String()
+	v.Memory = parseInt(tempMem)
+	v.Network = vm.Spec.Template.Spec.Networks[0].Name
+
+	for _, d := range vmi.Status.VolumeStatus {
+		if d.PersistentVolumeClaimInfo != nil {
+
+			v.Disk += parseInt(d.PersistentVolumeClaimInfo.Capacity.Storage().String())
+		}
+	}
+
+	v.GuestOS = vmi.Status.GuestOSInfo.PrettyName
+	v.IP = vmi.Status.Interfaces[0].IP
+
+	return v
+}
+
+func parseInt(s string) int {
+	re := regexp.MustCompile("[0-9]+")
+	tS := re.FindAllString(s, 1)
+	tI, _ := strconv.Atoi(tS[0])
+	return tI
+}
+
+func httpVMList(w http.ResponseWriter, r *http.Request) {
+	GetVM(r.URL.String())
+	fmt.Fprint(w)
+}
